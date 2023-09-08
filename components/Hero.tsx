9cfa2c144e5ec2473cd62b036f3ecfc0727c40d8
tijnbarthen <tijn@barthen.com>
2023-09-08 18:37:33 +0200

    please make this work

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,24 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-
 export default function Hero() {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  useEffect(() => {
-    const video = videoRef.current;
-
-    if (video) {
-      video.play().catch((error: any) => {
-        // Handle any play() promise rejection (e.g., due to autoplay restrictions)
-        console.error('Video playback error:', error);
-      });
-    }
-  }, []);
-
   return (
     <div>
       <video
-        ref={videoRef}
         autoPlay={true}
+        preload="auto"
         muted
         loop
         controls={false}
